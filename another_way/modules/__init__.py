34baff4124a93bf47e13fed3f80b47935704f9eb
Nguyễn Anh Khoa <110364616+itsmekhoathekid@users.noreply.github.com>
2025-05-17 12:13:09 +0700

    Delete another_way directory

--- another_way/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .predicate_list_extractor import predicate_nl_extractor
-from .conclusion_making import make_conclusion, Extract_Hypothesis
-from .nl2fol import nl_to_fol
--- another_way/unzip.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-
-zip_path = "/data/npl/ViInfographicCaps/Contest/final_contest/another_way/stanford-corenlp-4.5.6.zip"
-extract_dir = "/data/npl/ViInfographicCaps/Contest/final_contest/another_way/"
-
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-    zip_ref.extractall(extract_dir)
-
-print("✅ Unzipped successfully.")
